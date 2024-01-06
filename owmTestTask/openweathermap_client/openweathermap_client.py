82809f22d0ee27b2ee19624de307ad9468961e3a
Oksana <kosar.oksana55@gmail.com>
2024-01-06 15:55:50 +0200

    Separation the ApiService from the client

--- owmTestTask/openweathermap_client/openweathermap_client.py
+++ owmTestTask/openweathermap_client/openweathermap_client.py
@@ -1,83 +1,24 @@
 """openweathermap_client.py module.
 
 This module provides a client for interacting with the OpenWeatherMap API to retrieve weather information.
-
 Classes:
-    ApiRequestHandler:
-        Handles API requests to the OpenWeatherMap API.
 
     OpenWeatherMapClient:
         A client for interacting with the OpenWeatherMap API.
 
-Attributes:
-    __version__ (str): The version of the OpenWeatherMapClient module.
-
 Example:
-    request_handler = ApiRequestHandler()
-    client = OpenWeatherMapClient(api_key='your_api_key', request_handler=request_handler)
-    current_weather = client.get_current_weather(lat='37.7749', lon='-122.4194')
-    forecast = client.get_five_days_forecast(lat='37.7749', lon='-122.4194')
+    api_key='your_api_key'
+    weather_service = WeatherApiService
+    request_handler = WeatherApiService(api_key)
+    client = OpenWeatherMapClient(weather_service)
+    current_weather = client.get_current_weather(lat='48.92', lon='24.71')
+    forecast = client.get_five_days_forecast(lat='48.92', lon='24.71')
 
 """
 
-import logging
 from typing import Any, Dict, Optional
 
-import requests
-
-
-class ApiRequestHandler(object):
-    """
-    Handles API requests to the OpenWeatherMap API.
-
-    Attributes:
-        endpoint_url (str): The base URL for OpenWeatherMap API endpoints.
-        timeout (int): Timeout for API requests.
-
-    Methods:
-        make_request(endpoint: str, params: Dict[str, str], api_key: str) -> Optional[Dict[str, Any]]:
-            Make a request to the OpenWeatherMap API.
-    """
-
-    def __init__(self, endpoint_url: str = 'https://api.openweathermap.org/data/2.5', timeout: int = 5) -> None:
-        """
-        Initialize the ApiRequestHandler.
-
-        Args:
-            endpoint_url (str): The base URL for OpenWeatherMap API endpoints.
-            timeout (int): Timeout for API requests.
-        """
-        self.endpoint_url = endpoint_url
-        self.timeout = timeout
-
-    def make_request(
-        self, endpoint: str, arguments: Dict[str, str], api_key: str,
-    ) -> Optional[Dict[str, Any]]:
-        """Make a request to the OpenWeatherMap API.
-
-        Args:
-            endpoint (str): The API endpoint.
-            arguments (Dict[str, str]): Parameters for the API request.
-            api_key (str): The API key required for authentication.
-
-        Returns:
-            Optional[Dict[str, Any]]: A dictionary containing the API response,
-                                      or None if the request was unsuccessful.
-        """
-        try:
-            response = requests.get(
-                '{0}/{1}'.format(self.endpoint_url, endpoint),
-                params={**arguments, 'appid': api_key},
-                timeout=self.timeout,
-            )
-        except requests.RequestException as exep:
-            logging.error('RequestException: {0}'.format(exep))
-            return None
-
-        if response.ok:
-            return response.json()
-        logging.error('Error: {0} - {1}'.format(response.status_code, response.text))
-        return None
+from owmTestTask.openweathermapapi_service.openweathermapapi_service import WeatherApiService
 
 
 class OpenWeatherMapClient(object):
@@ -85,7 +26,6 @@ class OpenWeatherMapClient(object):
     A client for interacting with the OpenWeatherMap API to retrieve weather information.
 
     Attributes:
-        api_key (str): The API key required for authentication.
         request_handler (ApiRequestHandler): Handler for making API requests.
 
     Methods:
@@ -96,17 +36,14 @@ class OpenWeatherMapClient(object):
             Get a five-day weather forecast based on latitude and longitude.
     """
 
-    def __init__(self, api_key: str, request_handler: ApiRequestHandler = None) -> None:
+    def __init__(self, weather_service: WeatherApiService = None) -> None:
         """
         Initialize the OpenWeatherMapClient.
 
         Args:
-            api_key (str): The API key required for authentication.
-            request_handler (ApiRequestHandler): Handler for making API requests.
+            weather_service (WeatherApiService): Service for making API requests.
         """
-        self.api_key = api_key
-        self.request_handler = request_handler or ApiRequestHandler()
-        self.logger = logging.getLogger(__name__)
+        self.weather_service = weather_service or WeatherApiService()
 
     def get_current_weather(self, lat: str, lon: str) -> Optional[Dict[str, Any]]:
         """
@@ -121,7 +58,7 @@ class OpenWeatherMapClient(object):
                                       or None if the request was unsuccessful.
         """
         coordinates = {'lat': lat, 'lon': lon}
-        return self.request_handler.make_request('weather', coordinates, self.api_key)
+        return self.weather_service.get_request('weather', coordinates)
 
     def get_five_days_forecast(self, lat: str, lon: str) -> Optional[Dict[str, Any]]:
         """
@@ -136,4 +73,4 @@ class OpenWeatherMapClient(object):
                                       or None if the request was unsuccessful.
         """
         coordinates = {'lat': lat, 'lon': lon}
-        return self.request_handler.make_request('forecast', coordinates, self.api_key)
+        return self.weather_service.get_request('forecast', coordinates)
---
+++ owmTestTask/openweathermapapi_service/__init__.py
@@ -0,0 +1,1 @@
+"""Package for Api service."""
